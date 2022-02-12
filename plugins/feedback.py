f965123653e64b3c3c9e1a8b579efaa5dede63f4
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-02-12 17:45:12 +0530

    Update feedback.py

--- plugins/feedback.py
+++ plugins/feedback.py
@@ -4,10 +4,10 @@ from sample_config import Config
 
 
 @Clinton.on_message(filters.command('feedback'))
-async def report(bot, message):
+async def report(client, message):
         if message.reply_to_message:
-                                  await bot.send_message(chat_id=Config.OWNER_ID, text=f"<b>⭕️Putarte Feedback⭕️\n \n🧿 Name: {message.from_user.mention}\n🧿 User ID:</b> <code>{message.chat.id}</code>")
-                                  await bot.forward_messages(chat_id=Config.OWNER_ID, from_chat_id=message.from_user.id, message_ids=message.reply_to_message.message_id)
-                                  await message.reply_text("<b>✅ I feedback chu min siamtu hnen ah thawn ani e.</b>")
+            await client.send_message(chat_id=Config.OWNER_ID, text=f"<b>⭕️Feedback⭕️\n \n🧿 Name: {message.from_user.mention}\n🧿 ID:</b> <code>{message.chat.id}</code>")
+            await client.forward_messages(chat_id=Config.OWNER_ID, from_chat_id=message.from_user.id, message_ids=message.reply_to_message.message_id)
+            await message.reply_text("<b>✅ Feedback was send</b>")
         else:
-             await message.reply_text("<b>Command hmang hian i message thawn tur reply tur.</b>")
+            await message.reply_text("<b>Reply feedback message with command.</b>")
