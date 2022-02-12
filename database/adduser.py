8c5a16410fa1ede06e7d8849b9909612f493af4e
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-02-12 19:42:16 +0530

    Update adduser.py

--- database/adduser.py
+++ database/adduser.py
@@ -3,11 +3,11 @@ from database.imm import rsr
 from pyrogram.types import Message
 from config import Config
 
-async def AddUser(bot: Client, update: Message):
-    if not await clinton.is_user_exist(update.from_user.id):
-           await clinton.add_user(update.from_user.id)
+async def AddUser(client: Client, message: Message):
+    if not await rsr.is_user_exist(message.from_user.id):
+           await rsr.add_user(message.from_user.id)
            if Config.LOG_CHANNEL is not None:
-            await bot.send_message(
+            await client.send_message(
                 int(Config.LOG_CHANNEL),
-                f"#NEW_USER \n\n➥**Name:** [{update.from_user.first_name}](tg://user?id={update.from_user.id})\n\n➥**ID:** {update.from_user.id}"
+                f"#NEW_USER \n\n➥**Name:** [{message.from_user.first_name}](tg://user?id={message.from_user.id})\n\n➥**ID:** {message.from_user.id}"
             ) 
