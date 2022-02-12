06bca2de7cb0e0c5ac0f35254515d16917fad162
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-02-12 17:15:01 +0530

    Add files via upload

---
+++ translation.py
@@ -0,0 +1,29 @@
+class Translation(object):
+    START_TEXT = """Hello {}\n
+Kei hi URL uploader Bot ka ni e.\n\nMin hman dan i hriat duh chuan i keyboard awmna button a **Tutorial** tih kha i hmet mai dawn nia.\n\n**Link Support te:** Direct Download link leh youtube-dl support ami te, youtube-dl support ho i en duh chuan [Min hmet rawh](https://ytdl-org.github.io/youtube-dl/supportedsites.html)\n(youtube-dl support ami ṭhenkhat hi tun ah rih chuan download/upload theih ala ni rih lo)\n\n**Note:** Audio leh Video chiah ka download in ka thawn thei."""
+    FORMAT_SELECTION = "<b>A hnuai a button ho ah khuan i download duh dan thlang rawh</b>\n\n📽️ telna kha Video pangai ania, 📂 telna kha Document file ani. Tin, 🎵 lan na kha Audio ani\n\n"
+    SET_CUSTOM_USERNAME_PASSWORD = """Harsatna i neih chuan @helptereuhte ah hian ilo sawi dawn nia"""
+    DOWNLOAD_START = "**Download mek e**"
+    UPLOAD_START = "**Upload mek e\n**"
+    RCHD_TG_API_LIMIT = "**Download chhung:** {} second\n\n**File Size:** {}\n\n**2GB aia size lian ka upload theilo**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Min hman avangin kan lawm e😊**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "**Download chhung:** {} second\n\n**Upload chhung:** {} second"
+    SAVED_CUSTOM_THUMB_NAIL = "**Custom thumbnail siam ani**"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "**Custom thumbnail paih ani**"
+    CUSTOM_CAPTION_UL_FILE = "{}"
+    NO_VOID_FORMAT_FOUND = "ERROR: {}"
+    HELP_USER = """**Min hman dan:**
+    
+1. Direct download link emaw YouTube video link min rawn thawn ang.
+2. Ka rawn thawn dan tur che button ah khan duh i thlang ang.
+3. Download leh loh i duh chuan i close thei.
+4. 📽️ telna kha video pangai ania, 📂 telna kha document file ani.
+
+**Note:** Audio leh Video chiah ka download in ka thawn thei.
+
+Harsatna i neih chuan a hnuai a **Helpline** tih button kha hmet la join rawh, tah khan i rawn zawt dawn nia."""
+    REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "/generatecustomthumbnail tih hmang hian reply rawh"
+    ERR_ONLY_TWO_MEDIA_IN_ALBUM = """Harsatna a awm tlat @helptereuhte ah hian a chhan lo sawi rawh."""
+    CANCEL_STR = "Cancel ani"
+    ZIP_UPLOADED_STR = "{} zat file hi {} second chhung a upload ani"
+    SLOW_URL_DECED = "**URL ah dik tawklo a awm nge ni, a muang rih tlat.**"
