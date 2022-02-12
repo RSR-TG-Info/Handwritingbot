0dfbd8ca592c2f9cd5bdbae0870a10d35e453dab
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-02-12 19:32:18 +0530

    Update donate.py

--- plugins/donate.py
+++ plugins/donate.py
@@ -4,7 +4,7 @@ from pyrogram import Client as RSR
 
 
 
-@RSR.on_message(filters.command("donate")
+@RSR.on_message(filters.command("donate"))
 async def donate(client, message):
     await client.forward_messages(message.chat.id, from_chat_id=-1001640035413, message_ids=30)
   
