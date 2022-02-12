115e129972b667c1532461eb776477fcb991526c
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-02-12 18:00:35 +0530

    Update and rename help_text.py to main.py

--- plugins/main.py
+++ plugins/main.py
@@ -1,8 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) Shrimadhav U K
-
-# the logging things
+# ©️2022 RSR
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -11,9 +7,9 @@ logger = logging.getLogger(__name__)
 import os
 import sqlite3
 
-# the secret configuration specific things
+
 if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config
+    from config import Config
 else:
     from config import Config
 
@@ -22,37 +18,33 @@ from translation import Translation
 
 from pyrogram import filters
 from database.adduser import AddUser
-from pyrogram import Client as Clinton
+from pyrogram import Client as RSR
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
 
 
-@Clinton.on_message(filters.group & filters.command(["help"]))
-async def help_user(bot, update):
-    # logger.info(update)
+@RSR.on_message(filters.command(["help"]))
+async def help_user(client, message):
     await AddUser(bot, update)
     rsr1 = [[
             InlineKeyboardButton("Helpline", url="https://t.me/helptereuhte")
         ]]
-    await bot.send_message(
-        chat_id=update.chat.id,
+    await client.send_message(
+        chat_id=message.chat.id,
         text=Translation.HELP_USER,
         reply_markup=InlineKeyboardMarkup(rsr1),
         parse_mode="markdown",
-        disable_web_page_preview=True,
-        reply_to_message_id=update.message_id
+        reply_to_message_id=message.message_id
     )
 
 
-@Clinton.on_message(filters.private & filters.command(["start"]))
-async def start(bot, update):
-    # logger.info(update)
-    await AddUser(bot, update)
-    await bot.send_message(
-        chat_id=update.chat.id,
+@Clinton.on_message(filters.command(["start"]))
+async def start(client, message):
+    await AddUser(client, message)
+    await client.send_message(
+        chat_id=message.chat.id,
         text=Translation.START_TEXT.format(update.from_user.mention),
-        disable_web_page_preview=True,
         reply_markup=ReplyKeyboardMarkup(
             [
                 ["Tutorial"],
