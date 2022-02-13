457ce58e53aa9a6154e5f3456b80e26e6f6efc48
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-02-13 12:55:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,5 +24,5 @@ if __name__ == "__main__" :
         bot_token=Config.TG_BOT_TOKEN,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
-        plugins=plugins)
+        plugins=RSR.plugins)
     hmm.run()
