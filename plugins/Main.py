0ce66f0ce5e80c77e2a552f5096550a739a5a0ad
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-02-20 16:58:08 +0530

    Update Main.py

--- plugins/Main.py
+++ plugins/Main.py
@@ -21,17 +21,15 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
-@RSR.on_message(filters.command(["help"]))
+@RSR.on_message(filters.private & filters.command(["help"]))
 async def help_user(client, message):
     await AddUser(client, message)
     rsr1 = [[
-            InlineKeyboardButton("Support", url="https://t.me/helptereuhte"),
-            ],[
-            InlineKeyboardButton("Channel", url="https://t.me/rsrbots")
-        ]]
+            InlineKeyboardButton("Developer", user_id="1060318977"),
+            ]]
     await client.send_message(
         chat_id=message.chat.id,
-        text="<b>Private:</b>\n\n● Send me song name or YouTube video link, i will download and upload for you.\n● Send me Video or Audio, i will recognise.\n\n<b>Group:</b>\n\n● Send me song name or YouTube video link after command, command is /down\n<b>Example:</b>\n/down Marshmello - Alone\nor\n/down <code>https://youtu.be/ALZHF5UqnU4</code>\n\n● Send me Video or Audio. Then, reply your vedio/audio with command, command is /audify.",
+        text="Send me a text, i'll convert to Handwriting",
         reply_markup=InlineKeyboardMarkup(rsr1),
         parse_mode="html",
         disable_web_page_preview=True,
@@ -39,21 +37,18 @@ async def help_user(client, message):
     )
 
 
-@RSR.on_message(filters.command(["start"]))
+@RSR.on_message(filters.private & filters.command(["start"]))
 async def start(client, message):
     await AddUser(client, message)
     rsr2 = [[
-            InlineKeyboardButton("Help", callback_data="help")
-            ],[
-            InlineKeyboardButton("Support", url="https://t.me/helptereuhte"),
-            InlineKeyboardButton("Channel", url="https://t.me/rsrbots")
+            InlineKeyboardButton("Developer", user_id="1060318977")
             ],[
             InlineKeyboardButton("About", callback_data="about"),
-            InlineKeyboardButton("Developer", user_id="1060318977")
+            InlineKeyboardButton("Source", url="https://github.com/RSR-TG-Info/Handwritingbot")
         ]]
     await client.send_message(
         chat_id=message.chat.id,
-        text="Hello {}\n\nI am YouTube uploader and song recogniser Bot. You can download YouTube videos and you can find song using me.".format(message.from_user.mention),
+        text="Hello {}\n\nI am Handwriting converter Bot, send me any text, i'll convert to Handwriting.".format(message.from_user.mention),
         reply_markup=InlineKeyboardMarkup(rsr2),
         reply_to_message_id=message.message_id
     )
