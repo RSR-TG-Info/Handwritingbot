6241bce7f22814f1cd77faa9a89ccb02c54ee9c2
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-02-20 15:55:58 +0530

    Update shazam.py

--- plugins/shazam.py
+++ plugins/shazam.py
@@ -1,43 +1,40 @@
 import os
 import time
 from json import JSONDecodeError
-
 import requests
-
-# import ffmpeg
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from MashaRoBot.pyrogramee.pluginshelper import edit_or_reply, fetch_audio
-from MashaRoBot import pbot
+from pyrogram import Client as RSR
 
 
-@pbot.on_message(filters.command("audify", prefixes=["/", "!"]))
-async def shazamm(client, message):
-    kek = await edit_or_reply(message, "`Load mek e...`")
+@RSR.on_message(filters.private & filters.audio & filters.video & filter.document)
+async def shazam(client, message):
+    kek = await edit_or_reply(message, "`Loading...`")
     if not message.reply_to_message:
-        await kek.edit("Audio emaw Video reply rawh")
+        await kek.edit("Reply Audio or Video")
         return
-    if os.path.exists("friday.mp3"):
-        os.remove("friday.mp3")
+    if os.path.exists("RSR.mp3"):
+        os.remove("RSR.mp3")
     kkk = await fetch_audio(client, message)
     downloaded_file_name = kkk
     f = {"file": (downloaded_file_name, open(downloaded_file_name, "rb"))}
-    await kek.edit("**Zawng mek...**")
+    await kek.edit("**Searching...**")
     r = requests.post("https://starkapi.herokuapp.com/shazam/", files=f)
     try:
         xo = r.json()
     except JSONDecodeError:
-        await kek.edit("`He hla hi ka hmu zo lo`")
+        await kek.edit("**Song not found**")
         return
     if not xo.get("success"):
-        await kek.edit("`He hla hi ka hmu zo lo`")
+        await kek.edit("**Song not found**")
         os.remove(downloaded_file_name)
         return
     xoo = xo.get("response")
     zz = xoo[1]
     zzz = zz.get("track")
     if not zzz:
-        await kek.edit("`He hla hi ka hmu zo lo`")
+        await kek.edit("**Song not found**")
         return
     nt = zzz.get("images")
     image = nt.get("coverarthq")
@@ -47,19 +44,14 @@ async def shazamm(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "Hmu tu", url="https://t.me/tereuhtex_bot"
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        "Channel", url="https://t.me/rsrbots"
+                        "Shazamer", url="https://t.me/tereuhtex_bot"
                     )
                 ],
             ]
         )
-    messageo = f"""<b><u>Hla hmuh ani ✅</b></u>\n
-<b>📁 Hla hming : </b> {title}\n
-<b>🎙️ Sa/Siamtu : </b>{by}
+    messageo = f"""<b><u>Song Shazamed ✅</b></u>\n
+<b>📁 Song name : </b> {title}\n
+<b>🎙️ Artist : </b>{by}
 """
     await client.send_photo(message.chat.id, image, messageo, reply_markup=rsrkeyboa, reply_to_message_id=message.reply_to_message.message_id, parse_mode="HTML")
     os.remove(downloaded_file_name)
